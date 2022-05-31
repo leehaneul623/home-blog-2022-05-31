d1ca6e03460d78bbc0d9c78a896e5b89fb15a66d
leehaneul623 <dyj03216@naver.com>
2022-05-31 18:02:18 +0900

    about 페이지 추가

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function AboutPage() {
+  return ( 
+    <main>
+    <title>자기소개</title>
+    <h1>자기소개 페이지 입니다.</h1>
+  </main>
+  );
+}
+
+export default AboutPage;
