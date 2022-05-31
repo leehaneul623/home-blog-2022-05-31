510e54693d9ce2990c961d02a75e299e29996c5d
leehaneul623 <dyj03216@naver.com>
2022-05-31 17:28:14 +0900

    세팅

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Home Page</title>
+      <h1>Home Page</h1>
+    </main>
+  )
+}
+
+export default IndexPage
